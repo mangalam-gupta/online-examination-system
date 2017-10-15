d262032a3e7492850b7786fc964a0a216ddd4f1c
Mangalam Gupta <b150073ee@nitsikkim.ac.in>
2017-10-15 16:31:06 +0530

    Update project.sql

--- project.sql
+++ project.sql
@@ -19,3 +19,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `user` (`name`, `gender`, `college`, `email`, `mob`, `password`) VALUES
+('Mangalam Gupta','M','NIT Sikkim','b150073ee@nitsikkim.ac.in','7550851438','d9e6762dd1c8eaf6d61b3c6192fc408d4d6d5f1176d0c29169bc24e71c3f274ad27fcd5811b313d681f7e55ec02d73d499c95455b6b5bb503acf574fba8ffe85'),
+('Annu','M','NIT Sikkim','mangalamgupta01@gmail.com','7550851438','d9e6762dd1c8eaf6d61b3c6192fc408d4d6d5f1176d0c29169bc24e71c3f274ad27fcd5811b313d681f7e55ec02d73d499c95455b6b5bb503acf574fba8ffe85'),
+('Test','M','NIT Sikkim','test@nitsikkim.ac.in','7550851438','d9e6762dd1c8eaf6d61b3c6192fc408d4d6d5f1176d0c29169bc24e71c3f274ad27fcd5811b313d681f7e55ec02d73d499c95455b6b5bb503acf574fba8ffe85'),
+('test2','M','NIT Sikkim','test2@nitsikkim.ac.in','7550851438','d9e6762dd1c8eaf6d61b3c6192fc408d4d6d5f1176d0c29169bc24e71c3f274ad27fcd5811b313d681f7e55ec02d73d499c95455b6b5bb503acf574fba8ffe85'),
