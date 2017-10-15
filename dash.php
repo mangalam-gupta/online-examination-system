9346ffaf9a24dbf023b67e81567decd4f9854308
Mangalam Gupta <b150073ee@nitsikkim.ac.in>
2017-10-15 12:37:51 +0530

    Update dash.php

--- dash.php
+++ dash.php
@@ -288,14 +288,14 @@
 						<div class="form-group">
 							<label class="col-md-12 control-label" for="'.$i.'3"></label>  
 								<div class="col-md-12">
-									<input id="'.$i.'3" name="'.$i.'3" placeholder="Enter option d" class="form-control input-md" type="text">
+									<input id="'.$i.'3" name="'.$i.'3" placeholder="Enter option c" class="form-control input-md" type="text">
     
 								</div>
 						</div>
 						<div class="form-group">
 							<label class="col-md-12 control-label" for="'.$i.'4"></label>  
 								<div class="col-md-12">
-									<input id="'.$i.'4" name="'.$i.'4" placeholder="Enter option c" class="form-control input-md" type="text">
+									<input id="'.$i.'4" name="'.$i.'4" placeholder="Enter option d" class="form-control input-md" type="text">
     
 								</div>
 						</div>
